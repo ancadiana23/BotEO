79d6c4bfd685309f3e4894fc4f4703d9733d1485
andra-maria <andra.patrascanu@yahoo.com>
2015-10-31 20:14:03 +0200

    Compiles

--- bot.h
+++ bot.h
@@ -1,5 +1,9 @@
-readMatrix(uint32 matrix[32][32], int&n, int&m);
+#include <stdint.h>
 
-calculateChainReaction(uint32 matrix[32][32], int n, int m);
+void readMatrix(uint32_t matrix[32][32], int&n, int&m);
 
-calculateFlameTimers(uint32 matrix[32][32], int n, int m, char flameTimer[32][32]);
+void calculateChainReaction(uint32_t matrix[32][32], int n, int m);
+
+void calculateFlameTimers(uint32_t matrix[32][32], int n, int m, char flameTimer[32][32]);
+
+void initializeExistRoutes(uint32_t matrix[32][32], int n, int m, char exitRoutes[32][32]);
