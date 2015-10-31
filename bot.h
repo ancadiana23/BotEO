54db0ff0894794b3e95aa93495ff9c6ce1a1d58a
andra-maria <andra.patrascanu@yahoo.com>
2015-10-31 20:08:47 +0200

    ok am facut prostii :D

--- bot.h
+++ bot.h
@@ -1,5 +1,5 @@
-readMatrix(int32_t matrix[32][32], int&n, int&m);
+readMatrix(uint32 matrix[32][32], int&n, int&m);
 
-calculateChainReaction(int32_t matrix[32][32], int n, int m);
+calculateChainReaction(uint32 matrix[32][32], int n, int m);
 
-calculateFlameTimers(int32_t matrix[32][32], int n, int m, char flameTimer[32][32]);
+calculateFlameTimers(uint32 matrix[32][32], int n, int m, char flameTimer[32][32]);
