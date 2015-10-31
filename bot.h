ca6d51c56a4f057abc68e641b3f6ecd7ddf3c05d
andra-maria <andra.patrascanu@yahoo.com>
2015-10-31 21:57:45 +0200

    Pus chesti globale, alea alea, sper sa fie bine, nu e nimic implementat, futu-i

--- bot.h
+++ bot.h
@@ -1,9 +1,27 @@
 #include <stdint.h>
+#include <vector>
 
-void readMatrix(uint32_t matrix[32][32], int&n, int&m);
+typedef struct {
+	int x, y, weight;
+	node* parent; //don't forget to allocate
+	std::vector node* kids; //don't forget to allocate, or I will find you and free you
+} node;
 
-void calculateChainReaction(uint32_t matrix[32][32], int n, int m);
+node rootNode;
 
-void calculateFlameTimers(uint32_t matrix[32][32], int n, int m, char flameTimer[32][32]);
+int n, m;
+uint32_t matrix[32][32];
+char flameTimer[32][32];
+node* corr[32][32];
 
-void initializeExistRoutes(uint32_t matrix[32][32], int n, int m, char exitRoutes[32][32]);
+void readMatrix();//uint32_t matrix[32][32], int& n, int& m
+
+void calculateChainReaction();// uint32_t matrix[32][32], int n, int m, char flameTimer[32][32]
+
+void calculateFlameTimers(); //uint32_t matrix[32][32], int n, int m, char flameTimer[32][32]
+
+void initializeRoutes(); //uint32_t matrix[32][32], int n, int m,  node* corr[32][32], node rootNode
+
+void playNormal();
+
+void playAggresive(); //okay nu stiu inca diferenta prea tare dar ceva imi spune ca o sa avem nevoie
